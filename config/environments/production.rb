71c12368ae65c9aea3262852f9184adcd31adb8c
Phil Hurwitz <pennstatephil@gmail.com>
2013-03-23 13:59:38 -0600

    end of adding users with devise lesson

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ Omrails::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  #devise mail stuff
+  config.action_mailer.default_url_options = { :host => 'http://floating-spire-6077.herokuapp.com:3000' }
+
 end
